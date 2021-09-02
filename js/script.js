be9f775d84d3d62e03ea93d116f4a51c804e3a28
Stefano Meza <pieromezatejada14@hotmail.com>
2021-09-02 00:35:10 -0500

    scrolling animation in JS

--- js/script.js
+++ js/script.js
@@ -4,3 +4,30 @@ const header = document.querySelector('.header');
 btnMenuNav.addEventListener("click", function () {
     header.classList.toggle("nav-open");
   });
+
+//Scroll animation
+  const allLinks = document.querySelectorAll('a:link');
+
+  allLinks.forEach(function(link){
+    link.addEventListener('click', function(e){
+      e.preventDefault();
+      const href = link.getAttribute("href");
+
+      //scroll back to the top
+      if (href==="#") 
+      window.scrollTo({
+        top:0,
+        behavior: "smooth",
+      });
+      // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+    });
+  });
+
