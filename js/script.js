309c9368ffd0d88ad0babc4eb1bdbc14290d1c5a
Stefano Meza <pieromezatejada14@hotmail.com>
2021-09-01 23:21:52 -0500

    Finishing mobile nav funciontality with JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-const menuNav = document.querySelector('.icon-mobile-nav');
+const btnMenuNav = document.querySelector('.btn-mobile-nav');
 const header = document.querySelector('.header');
 
-menuNav.addEventListener('click', function(){
-    header.classList.toggle('nav-open')
-    console.log('a')
-})
-
+btnMenuNav.addEventListener("click", function () {
+    header.classList.toggle("nav-open");
+  });
